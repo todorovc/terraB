ba11cdcf61ef424d347ec99730bed0a7c8e887e4
todorovc <todorovc@gmail.com>
2020-04-19 20:56:15 +0200

    output

--- main.tf
+++ main.tf
@@ -2,11 +2,35 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+
+ # Allow any 2.x version of the AWS provider
+  version = "~> 2.0"
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-03d8059563982d7b0"
-  instance_type = "t2.micro"
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+
+  name = var.security_group_name
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
